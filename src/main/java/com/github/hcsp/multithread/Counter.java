2ac29d346738c3ea3c5dc2dcc4f1cab2c64d37a1
kuanjiang2077 <53144803+kuanjiang2077@users.noreply.github.com>
2019-09-28 16:31:11 +0800

    Update Counter.java

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -3,18 +3,18 @@ package com.github.hcsp.multithread;
 public class Counter {
     private int value = 0;
 
-    public int getValue() {
+    public synchronize int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
+    public synchronize int addAndGet(int i) {
         value += i;
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
+    public synchronize int minusAndGet(int i) {
         value -= i;
         return value;
     }
