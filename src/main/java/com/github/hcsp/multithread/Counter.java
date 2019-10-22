ce756031d39b13125d3b4363d8a5bde429cffac7
x13319 <1191838499@qq.com>
2019-10-22 20:56:48 +0800

    修改为加锁

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
+    Lock lock = new ReentrantLock();
     private int value = 0;
 
     public int getValue() {
@@ -9,13 +14,17 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
+        lock.lock();
         value += i;
+        lock.unlock();
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
+        lock.lock();
         value -= i;
+        lock.unlock();
         return value;
     }
 }
