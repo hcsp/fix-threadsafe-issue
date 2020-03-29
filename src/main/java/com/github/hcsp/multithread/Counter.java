03d753a09668d09a6db949d1abb215d1e4b17dae
walkerxin <1464193658@qq.com>
2020-03-29 10:38:24 +0800

    implement safe multithread using AtomicInteger

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,23 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
-    private int value = 0;
+    private AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.get();
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public synchronized int addAndGet(int i) {
-        value += i;
-        return value;
+    public int addAndGet(int i) {
+        value.getAndAdd(i);
+        return value.get();
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public synchronized int minusAndGet(int i) {
-        value -= i;
-        return value;
+    public int minusAndGet(int i) {
+        value.getAndAdd(-i);
+        return value.get();
     }
 }
