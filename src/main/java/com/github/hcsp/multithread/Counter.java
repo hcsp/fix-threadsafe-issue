3c1b472c21e1de7c5e63764df3c8f53d49af1403
DESKTOP-8VP2TRE\keliangyong <keliangyong@gmail.com>
2020-04-14 21:51:52 +0800

    ReentrantLock

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,28 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public synchronized int addAndGet(int i) {
+    public int addAndGet(int i) {
+        lock.lock();
         value += i;
+        lock.unlock();
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public synchronized int minusAndGet(int i) {
+    public int minusAndGet(int i) {
+        lock.lock();
         value -= i;
+        lock.unlock();
         return value;
     }
 }
