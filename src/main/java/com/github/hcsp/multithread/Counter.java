508a4f771e0730c04304dd82ea9813d69f0f79d6
fuduo <329794799@qq.com>
2020-05-19 00:39:29 +0800

    使用AtomicInteger锁解决

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,35 +1,23 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Counter {
-    private int value = 0;
-    private Lock lock = new ReentrantLock();
+    private AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.intValue();
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        lock.lock();
-        try {
-            value += i;
-            return value;
-        } finally {
-            lock.unlock();
-        }
+    public AtomicInteger addAndGet(int i) {
+        value.addAndGet(i);
+        return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        lock.lock();
-        try {
-            value -= i;
-            return value;
-        } finally {
-            lock.unlock();
-        }
+    public AtomicInteger minusAndGet(int i) {
+        value.addAndGet(-i);
+        return value;
     }
 }
