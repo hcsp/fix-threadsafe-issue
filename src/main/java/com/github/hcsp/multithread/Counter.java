50e782b4d82b478719aa7c1699ee357168b4e3cc
McDul <McDul19960103@gmail.com>
2020-06-23 20:16:52 +0800

    给方法上锁使得方法完整运行，从而保证线程的安全性

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -2,21 +2,18 @@ package com.github.hcsp.multithread;
 
 public class Counter {
     private int value = 0;
-    private static final Object lock = new Object();
 
     public int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public synchronized int addAndGet(int i) {
+    public synchronized void addAndGet(int i) {
         value += i;
-        return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public synchronized int minusAndGet(int i) {
+    public synchronized void minusAndGet(int i) {
         value -= i;
-        return value;
     }
 }
