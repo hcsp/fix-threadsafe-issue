196d922e7c806ae2a1f249835c3b05698860d4d2
soohea <sooheaa@gmai.com>
2021-11-28 20:56:53 +0800

    fix-threadsafe-issue-2

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,7 +1,11 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
+
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
@@ -10,13 +14,26 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public synchronized int addAndGet(int i) {
-        value += i;
-        return value;
+        try {
+            lock.lock();
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
+
     }
 
     // 减去一个整数i，并返回减之后的结果
     public synchronized int minusAndGet(int i) {
-         value -= i;
-        return value;
+        try {
+            lock.lock();
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+
+        }
+
     }
 }
