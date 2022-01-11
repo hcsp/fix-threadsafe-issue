c8169324f296216fba33e995e8a9815a77538fee
TQ <756342530@qq.com>
2022-01-11 14:54:02 +0800

    使用 Lock 保证线程安全

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,38 @@
 package com.github.hcsp.multithread;
 
-public  class  Counter {
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Counter {
     private int value = 0;
 
     public int getValue() {
         return value;
     }
+    Lock lock = new ReentrantLock();
+
 
     // 加上一个整数i，并返回加之后的结果
-    public synchronized int addAndGet(int i) {
-        value += i;
-        return value;
+    public int addAndGet(int i) {
+        lock.lock();
+            try {
+                value += i;
+                return value;
+            } finally {
+                lock.unlock();
+            }
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public synchronized int minusAndGet(int i) {
-        value -= i;
-        return value;
+    public  int minusAndGet(int i) {
+        lock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
+
     }
 }
