49352554a71751b8bb08ae4e9fa4765d41ee7296
Leo Cheung <leozhang621@gmail.com>
2024-01-11 17:32:08 +0800

    feat: use ReentrantLock to solve thread insecurity problems

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,35 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Counter {
-    private AtomicInteger value = new AtomicInteger(0);
+    private int value = 0;
+    private final Lock lock = new ReentrantLock();
 
     public int getValue() {
-        return value.intValue();
+        return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        return value.addAndGet(i);
+        lock.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        return value.addAndGet(-i);
+        lock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 }
